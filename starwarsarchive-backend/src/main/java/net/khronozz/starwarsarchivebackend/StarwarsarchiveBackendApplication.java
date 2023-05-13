714968600113dad56db4124c3397af9c2f56cf8d
Nicolas Favre <khronozz-dev@proton.me>
2023-05-14 00:43:09 +0200

    Add Angular and Spring base files

---
+++ starwarsarchive-backend/src/main/java/net/khronozz/starwarsarchivebackend/StarwarsarchiveBackendApplication.java
@@ -0,0 +1,13 @@
+package net.khronozz.starwarsarchivebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsarchiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsarchiveBackendApplication.class, args);
+	}
+
+}
---
+++ starwarsarchive-backend/src/test/java/net/khronozz/starwarsarchivebackend/StarwarsarchiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.khronozz.starwarsarchivebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsarchiveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
